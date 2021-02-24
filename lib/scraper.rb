a10c7394a30472c5216804c5d33689de80271c2a
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-24 06:04:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,18 +6,40 @@ class Scraper
 
   def self.scrape_index_page(index_url) #scraping the index page that lists all of the students
     doc = Nokogiri::HTML(open(index_url))
+    binding.pry
     scraped_students = []
 
     doc.css("div.student-card").each do |student|
       scraped_students << {
         name: student.css("h4.student-name").text,
-        location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
+        location: student.css("p.student-location").text, 
+        profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
+        
     end
     scraped_students
   end
 
   def self.scrape_profile_page(profile_url) #scraping an individual student's profile page to personal information
+    doc = Nokogiri::HTML(open(profile_url))
 
+    student_details = {}
+    doc.css("div.social-icon-container").each do |social_media|
+   if social_media.include?("twitter")
+     student_details[:twitter] = doc.css("div.social-icon-containter")[0]["href"]
+   elsif social_media.include?("linkedin")
+     student_details[:linkedin] = doc.css("div.social-icon-containter")[0]["href"]
+   elsif social_media.include?("github")
+     student_details[:github] = doc.css("div.social-icon-containter")[0]["href"]
+   elsif social_media.include?(".com")
+     student_details[:blog]= doc.css("div.social-icon-container a")[0]["href"]
+   end
+ end 
+
+ student_details[:profile_quote] = doc.css("div.vitals-text-container.profile-quote").text
+ student_details[:bio] = doc.css("div.details-container.description-holder p").text
+
+ student_details
+ end
   end
 
 end
