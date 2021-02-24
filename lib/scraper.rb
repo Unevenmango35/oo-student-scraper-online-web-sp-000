f1a9aa1d3d2a355547a9b3a87ce1419eec35e95a
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-24 05:31:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,12 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #scraping the index page that lists all of the students
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile_url) #scraping an individual student's profile page to personal information
+
   end
 
 end
-
