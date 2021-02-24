d4901077d02ba08caa12339f4d71d2631c4d9b26
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-24 05:52:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,15 +6,14 @@ class Scraper
 
   def self.scrape_index_page(index_url) #scraping the index page that lists all of the students
     doc = Nokogiri::HTML(open(index_url)
-    doc.css("div.roster-cards-container").each do |card|
-     card.css(".student-card a").each do |student|
-       name = student.css(".student-name").text
-       location = student.css(".student-location").text
-       profile_url = "#{student.attr("href")}"
-       students << {name: name, location: location, profile_url: profile_url}
-     end
-   end
-   students
+    scraped_students = []
+
+    doc.css("div.student-card").each do |student|
+      scraped_students << {
+        name: student.css("h4.student-name").text, 
+        location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
+    end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url) #scraping an individual student's profile page to personal information
